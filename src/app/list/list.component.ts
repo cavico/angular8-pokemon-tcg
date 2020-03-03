1c2d2336ce182b473167c490703a1226f30e2a00
Camilo Vitorino <camilo@cavico.com.br>
2020-03-02 23:47:15 -0300

    cards list is now sorted by name

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { CardsService } from 'src/app/services/cards.service';
 import { Card } from 'src/app/common/interface';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-list',
@@ -15,11 +16,23 @@ export class ListComponent {
   constructor(
     private cardsService: CardsService
   ) {
-    this.items$ = cardsService.getAll('Pokémon');
+    this.items$ = cardsService.getAll('Pokémon')
+      .pipe(
+        map(
+          (data: Card[]) => {
+            data.sort(this.sortByName);
+            return data;
+          }
+        )
+      );
   }
 
   trackByFn(index: number, item: Card) {
     return item.id;
+  }
+
+  sortByName(a: Card, b: Card) {
+    return a.name < b.name ? -1 : 1;
   }
 
 }
