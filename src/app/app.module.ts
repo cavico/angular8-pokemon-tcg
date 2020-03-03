35354b29fe2d66c2f878ed6c6835c96f857dae25
Camilo Vitorino <camilo@cavico.com.br>
2020-03-03 00:09:18 -0300

    list-item app.module added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { ListComponent } from './list/list.component';
 import { DetailComponent } from './detail/detail.component';
+import { ListItemComponent } from './list-item/list-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ListComponent,
-    DetailComponent
+    DetailComponent,
+    ListItemComponent
   ],
   imports: [
     BrowserModule,
